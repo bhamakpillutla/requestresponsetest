e1735465520063ffd4f1fe4116512d75df5ce801
Bhama Pillutla <65846493+bhamakpillutla@users.noreply.github.com>
2023-05-26 14:18:10 -0500

    Create rptest.py

---
+++ rptest.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    response = {
+            'message': 'Hello, world!',
+            'status': 'success'
+            }
+    time.sleep(2) # Simulate a 2-second delay
+
+    # Intentionally generate bad responses
+    response['message']= 'Internal Server Error'
+    response['status'] = 'error'
+    return jsonify(response), 500
+
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0', port = 8000)
