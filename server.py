ce2b79f5d06fce1600c8d23ad009dfaf5007564a
Bhama Pillutla <65846493+bhamakpillutla@users.noreply.github.com>
2023-11-29 16:40:28 -0800

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,6 @@ def hello():
             }
     # time.sleep(2) # Simulate a 2-second delay
     app.logger.info(response)
-    logging.basicConfig(level=logging.INFO)
     # Intentionally generate bad responses
     response['message']= 'Internal Server Error'
     response['status'] = 'error'
@@ -20,4 +19,5 @@ def hello():
 
 if __name__=='__main__':
     # time.sleep(120)
-    app.run(debug=True,host='0.0.0.0', port = 7000)
+    logging.basicConfig(level=logging.INFO)
+    app.run(debug=False,host='0.0.0.0', port = 7000)
